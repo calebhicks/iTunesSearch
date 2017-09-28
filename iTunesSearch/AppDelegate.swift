7f621b8d8caf15b3379cd816d0696859fb107bb1
Caleb Hicks <calebhicks@gmail.com>
2017-09-28 11:44:25 -0600

    adds larger cache to support larger images

--- iTunesSearch/AppDelegate.swift
+++ iTunesSearch/AppDelegate.swift
@@ -18,6 +18,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
+        let temporaryDirectory = NSTemporaryDirectory()
+        let urlCache = URLCache(memoryCapacity: 25000000, diskCapacity: 50000000, diskPath: temporaryDirectory)
+        URLCache.shared = urlCache
+        
         return true
     }
 
